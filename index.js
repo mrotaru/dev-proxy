3fb9ca496a1d24ecf1c3a9d894fd8ba6800b8ce3
Mihai Rotaru <mihai.rotaru@gmx.com>
2018-07-14 23:39:47 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const http = require('http')
+const { URL } = require('url')
+const assert = require('assert')
+
+const DevProxy = {
+  init,
+  listen,
+  onListening() {},
+}
+
+function init ({ target, encoding = 'utf8', closeAfterFirstRequest = false}) {
+  this.server = http.createServer()
+  this.history = []
+  const server = this.server
+
+  server.on('request', (req, res) => {
+    const { method, headers } = req
+    const url = new URL(req.url, target)
+    const { port, protocol, hostname } = url
+    const options = { method, headers, hostname, protocol, port, path: req.url, host: false }
+    const clientReq = http.request(options, clientRes => {
+      clientRes.setEncoding(encoding)
+      let resBody = ''
+      clientRes.on('data', chunk => {
+        resBody=`${resBody}${chunk}`
+      })
+      clientRes.on('end', () => {
+        res.writeHead(clientRes.statusCode, clientRes.headers)
+        res.end(resBody)
+        if (closeAfterFirstRequest) {
+          server.close()
+        }
+      })
+    })
+    clientReq.on('error', err => {
+      console.log('client error', err)
+    })
+    clientReq.end()
+  })
+  server.on('listening', () => {
+    this.onListening()
+  })
+  return this
+}
+
+function listen(port) {
+  assert(this.server)
+  this.server.listen(port)
+}
+
+module.exports = DevProxy
---
+++ test.js
@@ -0,0 +1,64 @@
+const http = require('http')
+const assert = require('assert')
+const DevProxy = require('.')
+
+const test = ({ serverResponse, clientRequest, encoding = 'utf8' }) => {
+  const testServer = http.createServer()
+  testServer.on('request', (req, res) => {
+    console.log(`${req.method} ${req.url}`)
+    assert(propsContained(clientRequest.headers, req.headers), 'request headers not passed to proxied server')
+    res.writeHead(serverResponse.statusCode, serverResponse.headers)
+    res.end(serverResponse.body)
+  })
+  testServer.listen(3001)
+  testServer.on('listening', () => {
+    const proxy = Object.create(DevProxy)
+    proxy.init({
+      target: 'http://localhost:3001',
+      closeAfterFirstRequest: true,
+    }).listen(3000)
+
+    proxy.onListening = function () {
+      const proxyRequest = http.request(clientRequest, proxyRes => {
+        let resBody = ''
+        proxyRes.setEncoding(encoding)
+        proxyRes.on('data', chunk => { resBody = `${resBody}${chunk}`})
+        proxyRes.on('end', () => {
+          assert(propsContained(serverResponse.headers, proxyRes.headers), 'proxied response headers not passed back')
+          assert(resBody === serverResponse.body, 'proxied response body is not passed back')
+          assert(proxyRes.statusCode === serverResponse.statusCode, 'statusCode is not equal')
+          testServer.close()
+        })
+      })
+      proxyRequest.end()
+    }
+  })
+}
+
+test({
+  clientRequest: {
+    port: 3000,
+    path: '/api/foo',
+    headers: {
+      'bar': 'bar-value'
+    }
+  },
+  serverResponse: {
+    statusCode: 200,
+    headers: {
+      'foo': 'foo-value'
+    },
+    body: '42'
+  },
+})
+
+const propsContained = (src, target) => {
+  let allContained = true
+  Object.keys(src).forEach(key => {
+    if (!target.hasOwnProperty(key) || target[key] !== src[key]) {
+      console.log('not contained', key, src[key], target[key])
+      allContained = false
+    }
+  })
+  return allContained
+}
